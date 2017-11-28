49fd693ef9df2079d8948de9bc12b14a5cce5d79
Luka <lratkajec@riteh.hr>
2017-11-28 07:30:09 -0500

    api 2

--- index.php
+++ index.php
@@ -2,7 +2,9 @@
 
 use Phalcon\Mvc\Micro;
 use Phalcon\Db\Adapter\Pdo\Mysql;
-require "./restclient.php.1";
+use Phalcon\Cache\Backend\Memcache;
+use Phalcon\Cache\Frontend\Data as FrontData;
+require "./restclient.php";
 
 $rc=new RestClient();
 
@@ -14,6 +16,20 @@ $config = [
     "username" => "ratkajec",
     "password" => "hexis1",
 ];
+
+$frontCache=new FrontData(
+	[
+		"lifetime" => 172800,
+	]
+);
+
+$cache=new Memcache(
+	$frontCache,
+	[
+		"host" => "localhost",
+		"persistent" => false,
+	]
+);
 
 $connection=new Mysql($config);
 
@@ -30,7 +46,7 @@ $app->get(
 
 $app->post(
 	'/api/insert',
-	function() use ($connection, $rc){
+	function() use ($connection, $rc, $cache){
 		$success = $connection->execute(
    			 "INSERT INTO Tweets(Title, Body, Date) VALUES (?, ?, now())",
 			 [
@@ -38,7 +54,8 @@ $app->post(
 				$_POST['body']
 			 ]
 		);
-		$rc->post("http://makovac.riteh.hexis.hr/api/insert", $_POST);
+		//$cache->save($connection->lastInsertId(), $_POST);
+		//$rc->post("http://makovac.riteh.hexis.hr/api/insert", $_POST);
 		$lastID=$connection->lastInsertId();
 		echo $lastID;
 	}
@@ -46,7 +63,7 @@ $app->post(
 
 $app->get(
 	'/api/printtweet/{ID}',
-	function($ID) use ($connection){
+	function($ID) use ($connection, $cache){
 		$resultset=$connection->fetchOne(
 			"SELECT * FROM Tweets where ID=:id",
 			\Phalcon\Db::FETCH_ASSOC,
@@ -55,6 +72,8 @@ $app->get(
 			]
 		);
 		print_r($resultset);
+		//$data=$cache->get($ID);
+		//echo $data;
 
 		echo "<form action='/api/update/".$ID."' method='POST'>";
 		echo '<input type="text" name="title" value="'.$resultset['Title'].'"><br>';
@@ -75,6 +94,49 @@ $app->post(
 			],
 			"ID = ".$ID
 		);
+	}
+);
+
+$app->get(
+	'/api/printall',
+	function() use ($connection){
+		$resultset=$connection->fetchAll(
+			"SELECT ID, Title FROM Tweets",
+			\Phalcon\Db::FETCH_ASSOC
+		);
+		echo json_encode($resultset);
+	}
+);
+
+$app->get(
+	'/api/details/{ID}',
+	function($ID) use ($connection){
+		$resultset=$connection->fetchOne(
+			"SELECT * FROM Tweets where ID=:id",
+			\Phalcon\Db::FETCH_ASSOC,
+			[
+				"id"=>$ID,
+			]
+		);
+		echo json_encode($resultset);
+	}
+);
+
+$app->get(
+	'/api/gettweets',
+	function() use ($rc, $connection){
+		$domains=array("negulic", "makovac");
+		$tweets=$rc->get("http://ratkajec.riteh.hexis.hr/api/printall");
+		$myTweets=json_decode($tweets->response, true);
+		print_r($myTweets);
+		echo "<br><br>";
+
+		$x=array();
+		foreach($domains as $domain){
+			$res=$rc->get("http://".$domain.".riteh.hexis.hr/api/printall");
+			$x[$domain]=json_decode($res->response, true);
+		}
+		print_r($x);
 	}
 );
 
