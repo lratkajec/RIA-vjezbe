569eca4305c1de19c2909ebeaf8df3e889a98ab1
Luka <lratkajec@riteh.hr>
2017-12-20 08:41:17 -0500

    oblikovni obrasci

--- index.php
+++ index.php
@@ -2,141 +2,117 @@
 
 use Phalcon\Mvc\Micro;
 use Phalcon\Db\Adapter\Pdo\Mysql;
-use Phalcon\Cache\Backend\Memcache;
-use Phalcon\Cache\Frontend\Data as FrontData;
-require "./restclient.php";
-
-$rc=new RestClient();
 
 $app=new Micro();
 
-$config = [
-    "host"     => "localhost",
-    "dbname"   => "ratkajec",
-    "username" => "ratkajec",
-    "password" => "hexis1",
-];
 
-$frontCache=new FrontData(
-	[
-		"lifetime" => 172800,
-	]
-);
+class Db{
+	public static $db;
 
-$cache=new Memcache(
-	$frontCache,
-	[
-		"host" => "localhost",
-		"persistent" => false,
-	]
-);
-
-$connection=new Mysql($config);
-
-$app->get(
-	'/api/tweets',
-	function(){
-		echo "<form action='/api/insert' method='POST'>";
-		echo "<input type='text' name='title'><br>";
-		echo "<input type='text' name='body'><br>";
-		echo "<input type='submit' value='Submit'>";
-		echo "</form>";
-	}
-);
-
-$app->post(
-	'/api/insert',
-	function() use ($connection, $rc, $cache){
-		$success = $connection->execute(
-   			 "INSERT INTO Tweets(Title, Body, Date) VALUES (?, ?, now())",
-			 [
-				$_POST['title'],
-				$_POST['body']
-			 ]
-		);
-		//$cache->save($connection->lastInsertId(), $_POST);
-		$rc->post("http://negulic.riteh.hexis.hr/api/insert", $_POST);
-		$lastID=$connection->lastInsertId();
-		echo $lastID;
-	}
-);
-
-$app->get(
-	'/api/printtweet/{ID}',
-	function($ID) use ($connection, $cache){
-		$resultset=$connection->fetchOne(
-			"SELECT * FROM Tweets where ID=:id",
-			\Phalcon\Db::FETCH_ASSOC,
-			[
-				"id"=>$ID,
-			]
-		);
-		print_r($resultset);
-		//$data=$cache->get($ID);
-		//echo $data;
-
-		echo "<form action='/api/update/".$ID."' method='POST'>";
-		echo '<input type="text" name="title" value="'.$resultset['Title'].'"><br>';
-		echo "<input type='text' name='body' value='".$resultset['Body']."'><br>";
-		echo "<input type='submit' value='Submit'>";
-		echo "</form>";
-	}
-);
-
-$app->post(
-	'/api/update/{ID}',
-	function($ID) use ($connection){
-		$success=$connection->updateAsDict(
-			"Tweets",
-			[
-				"Title" => $_POST['title'],
-				"Body" => $_POST['body'],
-			],
-			"ID = ".$ID
-		);
-	}
-);
-
-$app->get(
-	'/api/printall',
-	function() use ($connection){
-		$resultset=$connection->fetchAll(
-			"SELECT ID, Title FROM Tweets",
-			\Phalcon\Db::FETCH_ASSOC
-		);
-		echo json_encode($resultset);
-	}
-);
-
-$app->get(
-	'/api/details/{ID}',
-	function($ID) use ($connection){
-		$resultset=$connection->fetchOne(
-			"SELECT * FROM Tweets where ID=:id",
-			\Phalcon\Db::FETCH_ASSOC,
-			[
-				"id"=>$ID,
-			]
-		);
-		echo json_encode($resultset);
-	}
-);
-
-$app->get(
-	'/api/gettweets',
-	function() use ($rc, $connection){
-		$domains=array("negulic", "makovac");
-		$tweets=$rc->get("http://ratkajec.riteh.hexis.hr/api/printall");
-		$myTweets=json_decode($tweets->response, true);
-		print_r($myTweets);
-		echo "<br><br>";
-
-		$x=array();
-		foreach($domains as $domain){
-			$res=$rc->get("http://".$domain.".riteh.hexis.hr/api/printall");
-			$x[$domain]=json_decode($res->response, true);
+	public static function getInstance(){
+		if (null===self::$db){
+			self::$db=new Db();
+			self::$db->mysql=new Mysql(
+						[
+							"host"=>"localhost",
+    							"dbname"=>"ratkajec",
+        						"username"=>"ratkajec",
+            						"password"=>"hexis1",
+						]
+						);
 		}
-		print_r($x['makovac'][1]['ID']);
+		return self::$db;
+	}
+
+	public function rawQuery($query){
+		return self::$db->mysql->query($query);
+	}
+}
+
+class ArrayOperations{
+	public static $ao;
+	public static function getInstance(){
+		if(null===self::$ao){
+			self::$ao=new ArrayOperations();
+		}
+		return self::$ao;
+	}
+	public function getFirst($array){
+	}
+	public function getLast($array){
+	}
+}
+
+class HtmlBuilder{
+	public $currentHtml="html";
+	public $currentHead="head";
+	public $currentTitle="title";
+	public $currentBody="body";
+	public $currentParagraph="p";
+
+	public $page=NULL;
+	public $page_html=NULL;
+	public $page_head=NULL;
+	public $page_title=NULL;
+	public $page_body=NULL;
+	public $body_contents=NULL;
+
+	function addHtml(){
+		$this->page_html="<".$this->currentHtml.">";
+	}
+	function addHead(){
+		$this->page_head="<".$this->currentHead.">";
+	}
+	function addTitle(){
+		$this->page_title="<".$this->currentTitle.">";
+	}
+	function addBody(){
+		$this->page_body="<".$this->currentBody.">";
+	}
+	function addParagraph($string){
+		$this->body_contents.="<".$this->currentParagraph.">";
+		$this->body_contents.=$string;
+		$this->body_contents.="</".$this->currentParagraph.">";
+	}
+	function build(){
+		echo $this->page_html;
+		echo $this->page_head;
+		echo $this->page_title;
+		echo "</".$this->currentTitle.">";
+		echo "</".$this->currentHead.">";
+		echo $this->page_body;
+		echo $this->body_contents;
+		echo "</".$this->currentBody.">";
+		echo "</".$this->currentHtml.">";
+	}
+}
+
+class QueryBuilder{
+}
+
+function db(){
+	return Db::getInstance();
+}
+
+function strategy(){
+	return ArrayOperations::getInstance();
+}
+
+$app->get(
+	'/test',
+	function(){
+		$tweets=db()->rawQuery("select * from Tweets");
+
+
+		$html_builder=new HtmlBuilder();
+		$html_builder->addBody();
+		$html_builder->addParagraph("alalalalalal sfdsf");
+		$html_builder->addParagraph("bla bla");
+		$html_builder->addHtml();
+		$html_builder->addHead();
+		$html_builder->addTitle();
+
+		$html_builder->build();
 	}
 );
 
